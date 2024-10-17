e2d2bd806517a8a7f7f62814d1eb6696d0c12981
Miguel <39136142+LoveBetween@users.noreply.github.com>
2024-10-17 05:22:27 +0200

    remove the thang

--- main.lua
+++ main.lua
@@ -1,11 +1,11 @@
--- BigBrainTime v1.0.0
+-- BigBrainTime v2.0.0
 -- SmoothSpatula
 
 log.info("Successfully loaded ".._ENV["!guid"]..".")
 
-hooks = {}
-hooks["gml_Object_oStartMenu_Step_2"] = function() -- Init
-    hooks["gml_Object_oStartMenu_Step_2"] = nil
+mods["RoRRModdingToolkit-RoRR_Modding_Toolkit"].auto()
+
+local initialize = function()
     -- Duplicate ingame sprite, draw to surface with scale, save to new sprite, delete first sprite
     local scaling = 2
     local sprite = gm.sprite_duplicate(gm.constants.sBrain)
@@ -20,22 +20,16 @@ hooks["gml_Object_oStartMenu_Step_2"] = function() -- Init
     gm.sprite_delete(sprite)
 
     -- Set values (no localization)
-    gm.object_set_sprite_w(gm.constants.oBrain, big_brain_sprite)
-    local equipment = gm.variable_global_get("class_equipment")
-    for i=1, #equipment do
-        if equipment[i][2] == "rottenBrain" then
-            gm.array_set(equipment[i], 7, big_brain_sprite)
-            gm.array_set(equipment[i], 2, "It's Big Brain Time")
-            gm.array_set(equipment[i], 3, "Big Brain")
-        end
-    end
+    local Brain = Equipment.find("ror", "rottenBrain")
+    Brain:set_sprite(big_brain_sprite)
 end
 
-gm.pre_code_execute(function(self, other, code, result, flags)
-    if hooks[code.name] then
-        hooks[code.name](self)
-    end
-end)
+if hot_reloading then
+    initialize()
+else 
+    Initialize(initialize)
+end
+hot_reloading = true
 
 -- Set size and position of Brain Effect on creation
 gm.post_script_hook(gm.constants.instance_create, function(self, other, result, args)
@@ -47,6 +41,6 @@ end)
 -- Set damage and range of explosion
 gm.pre_script_hook(gm.constants.fire_explosion, function(self, other, result, args) -- scale 
     if other.object_name == "oEfBrain" then 
-        args[4].value, args[8].value, args[9].value  = 18, 2, 2.5 -- damage multiplier, width, height
+        args[4].value, args[8].value, args[9].value  = 20, 2, 2.5 -- damage multiplier, width, height
     end
 end)
